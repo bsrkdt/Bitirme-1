3d69ab79349c46e21856823ad378b889db105b39
Zeynep Yaren Oğuz <oguz.zeynepyaren@gmail.com>
2020-11-28 20:52:53 +0300

    Proje dosyası ekle.

---
+++ deneme/Seafood.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace deneme
+{
+    /// <summary>
+    /// Seafood.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Seafood : Window
+    {
+        public Seafood()
+        {
+            InitializeComponent();
+        }
+
+        private void Addition_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ deneme/ToastsandSandwiches.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace deneme
+{
+    /// <summary>
+    /// Toasts.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Toasts : Window
+    {
+        public Toasts()
+        {
+            InitializeComponent();
+        }
+
+        private void Addition_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
